fde16bf46d169a7d1504f63d567eb190543c455f
João Cardoso <contact@jaliborc.com>
2014-10-13 13:09:28 -0700

    No longer required "Trade Type"

--- sets.lua
+++ sets.lua
@@ -8,15 +8,9 @@
 local CombuctorSet = Combuctor:GetModule('Sets')
 local L = LibStub('AceLocale-3.0'):GetLocale('Combuctor')
 
-
---the all category (player, bagType filters)
-CombuctorSet:RegisterSubSet(L.All, L.All)
-CombuctorSet:RegisterSubSet(L.Normal, L.All, nil, function(player, bagType)
-	return bagType and bagType == 0
-end)
-CombuctorSet:RegisterSubSet(L.Trade, L.All, nil, function(player, bagType)
-	return bagType and bit.band(bagType, Combuctor.TRADE_TYPE) > 0
-end)
+CombuctorSet:RegisterSubSet(ALL, ALL)
+CombuctorSet:RegisterSubSet(L.Normal, ALL, nil, function(player, bagType) return bagType == 0 end)
+CombuctorSet:RegisterSubSet(L.Trade, ALL, nil, function(player, bagType) return bagType ~= 0 end)
 
 
 --equipment filters (armor, weapon, trinket)
@@ -24,7 +18,7 @@ local function isEquipment(player, bagType, name, link, quality, level, ilvl, ty
 	return (type == L.Armor or type == L.Weapon)
 end
 CombuctorSet:Register(L.Equipment, 'Interface/Icons/INV_Chest_Chain_04', isEquipment)
-CombuctorSet:RegisterSubSet(L.All, L.Equipment)
+CombuctorSet:RegisterSubSet(ALL, L.Equipment)
 
 local function isArmor(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
 	return type == L.Armor and equipLoc ~= 'INVTYPE_TRINKET'
@@ -53,7 +47,7 @@ local function isUsable(player, bagType, name, link, quality, level, ilvl, type,
 	end
 end
 CombuctorSet:Register(L.Usable, 'Interface/Icons/INV_Potion_93', isUsable)
-CombuctorSet:RegisterSubSet(L.All, L.Usable)
+CombuctorSet:RegisterSubSet(ALL, L.Usable)
 
 local function isConsumable(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
 	return type == L.Consumable
@@ -71,7 +65,7 @@ local function isQuestItem(player, bagType, name, link, quality, level, ilvl, ty
 	return type == L.Quest
 end
 CombuctorSet:Register(L.Quest, 'Interface/QuestFrame/UI-QuestLog-BookIcon', isQuestItem)
-CombuctorSet:RegisterSubSet(L.All, L.Quest)
+CombuctorSet:RegisterSubSet(ALL, L.Quest)
 
 
 --trade goods + gems
@@ -82,7 +76,7 @@ local function isTradeGood(player, bagType, name, link, quality, level, ilvl, ty
 	return type == L.Recipe or type == L.Gem
 end
 CombuctorSet:Register(L.TradeGood, 'Interface/Icons/INV_Fabric_Silk_02', isTradeGood)
-CombuctorSet:RegisterSubSet(L.All, L.TradeGood)
+CombuctorSet:RegisterSubSet(ALL, L.TradeGood)
 
 local function isTradeGood(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
 	return type == L.TradeGood
@@ -104,4 +98,4 @@ local function isMiscItem(player, bagType, name, link, quality, level, ilvl, typ
 	return type == L.Misc and (link:match('%d+') ~= '6265')
 end
 CombuctorSet:Register(L.Misc, 'Interface/Icons/INV_Misc_Rune_01', isMiscItem)
-CombuctorSet:RegisterSubSet(L.All, L.Misc)
+CombuctorSet:RegisterSubSet(ALL, L.Misc)
