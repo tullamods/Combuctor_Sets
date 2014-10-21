418b72d995cc5b8e6cba47680446c5637bcc9d15
João Cardoso <contact@jaliborc.com>
2014-10-20 22:11:42 -0700

    no message

--- sets.lua
+++ sets.lua
@@ -1,8 +1,6 @@
 --[[
-	Sets.lua
+	sets.lua
 		Basic set types for combuctor
-
-		setRule(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
 --]]
 
 local CombuctorSet = Combuctor:GetModule('Sets')
@@ -13,89 +11,94 @@ CombuctorSet:RegisterSubSet(L.Normal, ALL, nil, function(player, bagType) return
 CombuctorSet:RegisterSubSet(L.Trade, ALL, nil, function(player, bagType) return bagType ~= 0 end)
 
 
---equipment filters (armor, weapon, trinket)
-local function isEquipment(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+-- Equipment
+local function isEquipment(player, bagType, name, link, quality, level, ilvl, type)
 	return (type == L.Armor or type == L.Weapon)
 end
-CombuctorSet:Register(L.Equipment, 'Interface/Icons/INV_Chest_Chain_04', isEquipment)
-CombuctorSet:RegisterSubSet(ALL, L.Equipment)
 
 local function isArmor(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
 	return type == L.Armor and equipLoc ~= 'INVTYPE_TRINKET'
 end
-CombuctorSet:RegisterSubSet(L.Armor, L.Equipment, nil, isArmor)
 
-local function isWeapon(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+local function isWeapon(player, bagType, name, link, quality, level, ilvl, type)
 	return type == L.Weapon
 end
-CombuctorSet:RegisterSubSet(L.Weapon, L.Equipment, nil, isWeapon)
 
 local function isTrinket(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
 	return equipLoc == 'INVTYPE_TRINKET'
 end
+
+CombuctorSet:Register(L.Equipment, 'Interface/Icons/INV_Chest_Chain_04', isEquipment)
+CombuctorSet:RegisterSubSet(ALL, L.Equipment)
+CombuctorSet:RegisterSubSet(L.Armor, L.Equipment, nil, isArmor)
+CombuctorSet:RegisterSubSet(L.Weapon, L.Equipment, nil, isWeapon)
 CombuctorSet:RegisterSubSet(L.Trinket, L.Equipment, nil, isTrinket)
 
 
---usable items
-local function isUsable(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+-- Usables
+local function isUsable(player, bagType, name, link, quality, level, ilvl, type)
 	if type == L.Consumable then
 		return true
 	elseif type == L.TradeGood then
-		if subType == L.Devices or subType == L.Explosives then
-			return true
-		end
+		return subType == L.Devices or subType == L.Explosives
 	end
 end
-CombuctorSet:Register(L.Usable, 'Interface/Icons/INV_Potion_93', isUsable)
-CombuctorSet:RegisterSubSet(ALL, L.Usable)
 
-local function isConsumable(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+local function isConsumable(player, bagType, name, link, quality, level, ilvl, type)
 	return type == L.Consumable
 end
-CombuctorSet:RegisterSubSet(L.Consumable, L.Usable, nil, isConsumable)
 
-local function isDevice(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+local function isDevice(player, bagType, name, link, quality, level, ilvl, type)
 	return type == L.TradeGood
 end
+
+local function isQuestItem(player, bagType, name, link, quality, level, ilvl, type)
+	return type == L.Quest
+end
+
+CombuctorSet:Register(L.Usable, 'Interface/Icons/INV_Potion_93', isUsable)
+CombuctorSet:RegisterSubSet(ALL, L.Usable)
+CombuctorSet:RegisterSubSet(L.Consumable, L.Usable, nil, isConsumable)
 CombuctorSet:RegisterSubSet(L.Devices, L.Usable, nil, isDevice)
 
 
---quest items
-local function isQuestItem(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
-	return type == L.Quest
-end
+-- Questing
 CombuctorSet:Register(L.Quest, 'Interface/QuestFrame/UI-QuestLog-BookIcon', isQuestItem)
 CombuctorSet:RegisterSubSet(ALL, L.Quest)
 
 
---trade goods + gems
-local function isTradeGood(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+-- Trade Goods
+local function isTradeGood(player, bagType, name, link, quality, level, ilvl, type)
 	if type == L.TradeGood then
 		return not(subType == L.Devices or subType == L.Explosives)
 	end
+
 	return type == L.Recipe or type == L.Gem
 end
-CombuctorSet:Register(L.TradeGood, 'Interface/Icons/INV_Fabric_Silk_02', isTradeGood)
-CombuctorSet:RegisterSubSet(ALL, L.TradeGood)
 
-local function isTradeGood(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+local function isTradeGood(player, bagType, name, link, quality, level, ilvl, type)
 	return type == L.TradeGood
 end
-CombuctorSet:RegisterSubSet(L.TradeGood, L.TradeGood, nil, isTradeGood)
 
-local function isGem(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+local function isGem(player, bagType, name, link, quality, level, ilvl, type)
 	return type == L.Gem
 end
-CombuctorSet:RegisterSubSet(L.Gem, L.TradeGood, nil, isGem)
 
-local function isRecipe(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
+local function isRecipe(player, bagType, name, link, quality, level, ilvl, type)
 	return type == L.Recipe
 end
+
+CombuctorSet:Register(L.TradeGood, 'Interface/Icons/INV_Fabric_Silk_02', isTradeGood)
+CombuctorSet:RegisterSubSet(ALL, L.TradeGood)
+CombuctorSet:RegisterSubSet(L.TradeGood, L.TradeGood, nil, isTradeGood)
+CombuctorSet:RegisterSubSet(L.Gem, L.TradeGood, nil, isGem)
 CombuctorSet:RegisterSubSet(L.Recipe, L.TradeGood, nil, isRecipe)
 
---misc
-local function isMiscItem(player, bagType, name, link, quality, level, ilvl, type, subType, stackCount, equipLoc)
-	return type == L.Misc and (link:match('%d+') ~= '6265')
+
+-- Misc
+local function isMiscItem(player, bagType, name, link, quality, level, ilvl, type)
+	return type == L.Misc
 end
+
 CombuctorSet:Register(L.Misc, 'Interface/Icons/INV_Misc_Rune_01', isMiscItem)
 CombuctorSet:RegisterSubSet(ALL, L.Misc)
